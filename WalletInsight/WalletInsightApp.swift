9d8c00d1dbbc69a9a9a9cb74018d5e78f8a99ff6
Sivasankar <sivasankar@Promise-M3-Max-2.local>
2025-05-15 21:57:51 +0800

    HEAD - Commit UNO

---
+++ WalletInsight/WalletInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletInsightApp.swift
+//  WalletInsight
+//
+//  Created by Sivasankar on 15/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WalletInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
